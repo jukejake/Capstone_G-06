45fb1117da03355560769068f7eeac9f9cf409bf
jukejake <jacob.rosengren@uoit.net>
2018-11-11 18:44:46 -0500

    Added wheel rotation scripts
    +Added wheel rotation script.
    ~only added to the Muscle car so far.

---
+++ Capstone weather visualization/Assets/MY/Scripts/WheelController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
+
+public class WheelController : SerializedMonoBehaviour {
+
+    #region Variables
+    [Range(0,250)]
+    public int Speed;
+    private float WheelSize = 0.6884924f; //27.106 inches
+    public float RotateAmount { get; set; }
+    #endregion
+
+    #region Setup
+    private void Awake() {
+        RotateAmount = (Speed / WheelSize);
+        InvokeRepeating("DelayedUpdate", 1.0f, 0.50f);
+    }
+    void DelayedUpdate() {
+        RotateAmount = (Speed / WheelSize);
+    }
+    public void SetSpeed(Slider obj) {
+        Speed = (int)obj.value;
+    }
+    #endregion
+
+}
---
+++ Capstone weather visualization/Assets/MY/Scripts/WheelRotator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelRotator : MonoBehaviour {
+
+    #region Variables
+    WheelController WC;
+    private float RotateAmount;
+    public Transform[] LeftWheels;
+    public Transform[] RightWheels;
+    #endregion
+
+    #region Setup
+    // Use this for initialization
+    void Start () {
+        WC = FindObjectOfType<WheelController>();
+        RotateAmount = WC.RotateAmount;
+        InvokeRepeating("DelayedUpdate", 1.0f, 0.50f);
+    }
+	
+	void DelayedUpdate() {
+        RotateAmount = WC.RotateAmount;
+    }
+
+    private void FixedUpdate() {
+        foreach (var wheel in LeftWheels) {
+            wheel.Rotate(Vector3.left, RotateAmount);
+        }
+        foreach (var wheel in RightWheels) {
+            wheel.Rotate(Vector3.right, RotateAmount);
+        }
+    }
+
+
+    #endregion
+}
