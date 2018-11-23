51a04d6b9f82f8d202a7c526c2c5701e371a9a88
jukejake <jacob.rosengren@uoit.net>
2018-11-23 12:32:25 -0500

    Got ride of 8th wall
    Got ride of 8th wall

---
+++ Capstone weather visualization/Assets/MY/Scripts/FlowData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class FlowData : SerializedMonoBehaviour {
+
+	#region Variables
+	[Range(0.01f, 0.2f)]
+	public float StartSize = 0.1f;
+	[Range(1, 50)]
+	public int Rate = 5;
+
+	public bool Trail = true;
+
+	public GameObject[] Flows;
+	#endregion
+
+	#region Set Light data
+	[Button]
+	public void SetFlows() {
+		foreach (var item in Flows) {
+
+			var PS = item.GetComponent<ParticleSystem>();
+
+			var why0 = PS.main;
+			why0.startSize = StartSize;
+			
+			var why1 = item.GetComponent<ParticleSystem.EmissionModule>();
+			why1.rateOverDistance = Rate;
+
+			var why2 = item.GetComponent<ParticleSystem.TrailModule>();
+			why2.enabled = Trail;
+		}
+	}
+	#endregion
+}
