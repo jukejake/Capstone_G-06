835f932a8c4963fcaedf1f56e5b7604eca48c2a4
Aaron <aaronmac13@hotmail.com>
2019-01-14 21:03:26 -0500

    Weather Particles Effects and Dropdown Menu
    I added the Rain and Snow particle effects and it works using the weather buttons.
    I also added the UI - Modern Pack to the build to utilize the UI assets. I used it for the Dropdown Menu but it currently needs some tweaking but its in the scene, the other dropdown is just disabled in scene.

---
+++ Capstone weather visualization/Assets/Scripts/WeatherParticles.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherParticles : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> weatherList;
+
+    // Use this for initialization
+    void Awake()
+    {
+        foreach (GameObject weatherObject in weatherList)
+        {
+            weatherObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DisplayWeatherEffect(GameObject gameObject)
+    {
+        foreach (GameObject weatherObject in weatherList)
+        {
+            if (gameObject == weatherObject)
+            {
+                if (weatherObject.activeSelf)
+                    weatherObject.SetActive(false);
+                else
+                    weatherObject.SetActive(true);
+                //break;
+            }            
+            else
+                weatherObject.SetActive(false);
+        }
+    }
+}
