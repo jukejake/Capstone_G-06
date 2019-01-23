8b6cbb43046519dc097309293d1ba6c208dda26e
jukejake <jacob.rosengren@uoit.net>
2019-01-23 15:20:42 -0500

    Fix world loading and Moved Solar Array
    ~Fix world loading (Too slow)
    ~Moved Solar Array into Weather patterens

--- Capstone weather visualization/Assets/MY/Scripts/ButtonSceneData.cs
+++ Capstone weather visualization/Assets/MY/Scripts/ButtonSceneData.cs
@@ -12,12 +12,23 @@ public class ButtonSceneData : MonoBehaviour {
 	#region Variables
 	//A string for the scene name that the ChangeScene function will change to.
 	public string SceneName = "Default";
+	public GameObject[] HideWhenPressed;
+	public GameObject[] ShowWhenPressed;
 	#endregion
 
 	#region Functions
-	//Function to change the scene
+	//Function to change the unity scene
 	public void ChangeScene() {
 		SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+	}
+	//Function to hide and show parts of the scene.
+	public void SwitchScene() {
+		foreach (var item in HideWhenPressed) {
+			item.SetActive(false);
+		}
+		foreach (var item in ShowWhenPressed) {
+			item.SetActive(true);
+		}
 	}
 	#endregion
 }
