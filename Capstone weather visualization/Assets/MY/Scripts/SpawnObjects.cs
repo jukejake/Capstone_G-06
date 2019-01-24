fdbf7964796cc5d0f476b5a54f62baaa17bf7208
jukejake <jacob.rosengren@uoit.net>
2019-01-24 11:04:24 -0500

    Moveable Dyno
    The Dyno now moves based on the tire position.

--- Capstone weather visualization/Assets/MY/Scripts/SpawnObjects.cs
+++ Capstone weather visualization/Assets/MY/Scripts/SpawnObjects.cs
@@ -15,20 +15,34 @@ public class SpawnObjects : SerializedMonoBehaviour {
 	[OdinSerialize]
 	//A dictionary that has all the names and Gameobjects of the cars
 	public Dictionary<string, GameObject> IDTable = new Dictionary<string, GameObject>();
+	public Transform FrontDynos;
+	public Transform BackDynos;
 
 	//Function that will spawn a GameObject based on a key.
 	public void SpawnObject(string key) {
 		GameObject t;
 		if (IDTable.TryGetValue(key, out t)) {
+			//Remove the previous car.
 			Clear();
+			//Spawn the new car.
 			GameObject temp = Instantiate(t, this.transform.position, this.transform.rotation, this.transform);
 			temp.name = t.name;
+			//Set position of front Dynos.
+			var Cpos = temp.transform.position;
+			var CNewX = (FrontDynos.position.x - temp.transform.Find("Front Wheels").transform.position.x);
+			temp.transform.position = new Vector3(CNewX, Cpos.y, Cpos.z);
+			//Set position of back Dynos.
+			var Dpos = BackDynos.position;
+			var BW = temp.transform.Find("Back Wheels").transform.position.x;
+			BackDynos.position = new Vector3(BW, Dpos.y, Dpos.z);
 		}
 	}
 	//Function to clear all of the children on the Object
 	public void Clear() {
 		foreach (Transform child in this.transform) { Destroy(child.gameObject); }
 	}
+
+
 
 	/*
 	 Need to optimize the code;
