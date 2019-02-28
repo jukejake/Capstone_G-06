de279120f5279ac5ae426c155bc3ac109acb5fc5
jukejake <jacob.rosengren@uoit.net>
2019-02-28 14:47:24 -0500

    Updated lighting array
    ~Updated lighting array script and applyed to the scene

--- Capstone weather visualization/Assets/MY/Scripts/MovableLightingArray.cs
+++ Capstone weather visualization/Assets/MY/Scripts/MovableLightingArray.cs
@@ -18,30 +18,37 @@ public class MovableLightingArray : SerializedMonoBehaviour {
 	[InfoBox("Might need to ONLY have one Rotation active at a time.")]
 	[InfoBox("Movement Cap Y is expected to be negative.")]
 	public Dictionary<string, LightingState> LightingTable = new Dictionary<string, LightingState>();
+
+	private int Moving = 0;
+	private int Rotating = 0;
 	#endregion
 
 	#region Public Functions 
-	public void Select_Light(string key) {
-		LightingState t;
-		if (LightingTable.TryGetValue(key, out t)) { t.Selected = true; }
+
+	private void Update() {
+		if (Rotating != 0) {
+			RotateDirection(Rotating);
+		}
+		if (Moving != 0) {
+			MoveDirection(Moving);
+		}
 	}
 
-	public void Move_All_Lights(int _d)		 { MoveDirection(_d); } //0 = Up, 1 = Down, 2 = Inward, 3 = Outward.
-	public void Rotate_All_Lights(bool _r)	 { RotateDirection(_r); } //True = Inwards, False = Outwards.
+	public void Select_Light(string key) {
+		LightingState t;
+		if (LightingTable.TryGetValue(key, out t)) {
+			if (t.Selected) { t.Selected = false; }
+			else { t.Selected = true; }
+			LightingTable[key] = t;
+		}
+	}
 
-	public void Move_Front_Lights(int _d)	 { MoveDirection(_d); } //0 = Up, 1 = Down, 2 = Inward, 3 = Outward.
-	public void Move_Left_Lights(int _d)	 { MoveDirection(_d); } //0 = Up, 1 = Down, 2 = Inward, 3 = Outward.
-	public void Move_Middle_Lights(int _d)	 { MoveDirection(_d); } //0 = Up, 1 = Down, 2 = Inward, 3 = Outward.
-	public void Move_Right_Lights(int _d)	 { MoveDirection(_d); } //0 = Up, 1 = Down, 2 = Inward, 3 = Outward.
-	public void Move_Back_Lights(int _d)	 { MoveDirection(_d); } //0 = Up, 1 = Down, 2 = Inward, 3 = Outward.
-
-	public void Rotate_Front_Lights(bool _r) { RotateDirection(_r); } //True = Inwards, False = Outwards.
-	public void Rotate_Left_Lights(bool _r)	 { RotateDirection(_r); } //True = Inwards, False = Outwards.
-	public void Rotate_Right_Lights(bool _r) { RotateDirection(_r); } //True = Inwards, False = Outwards.
-	public void Rotate_Back_Lights(bool _r)	 { RotateDirection(_r); } //True = Inwards, False = Outwards.
+	public void Move_All_Lights(int _d)		 { Moving = _d; } //4 = Up, 1 = Down, 2 = Inward, 3 = Outward.
+	public void Rotate_All_Lights(int _r)	 { Rotating = _r; } //1 = Inwards, 2 = Outwards.
 	#endregion
 
 	#region Private Functions
+
 	[Button]
 	private void MoveDirection(int _d) {
 		float deltaT = (Time.deltaTime * MoveSpeed);
@@ -52,7 +59,7 @@ public class MovableLightingArray : SerializedMonoBehaviour {
 				var Local = light.Value.Light.transform.localPosition;
 				switch (_d) {
 					//Up
-					case 0:
+					case 4:
 						if (Local.y < 0) {
 							light.Value.Light.transform.Translate(0, deltaT, 0);
 						}
@@ -73,40 +80,40 @@ public class MovableLightingArray : SerializedMonoBehaviour {
 					case 2:
 						///X-axis
 						//Positive
-						if (light.Value.MovementCap.x > 0) { 
-							if (Local.x > 0) {
+						if (light.Value.MovementCap.x > light.Value.OGpos.x) { 
+							if (Local.x > light.Value.OGpos.x) {
 								light.Value.Light.transform.Translate(-deltaT, 0, 0);
 							}
-							if (Local.x < 0) {
-								light.Value.Light.transform.localPosition = new Vector3(0, Local.y, Local.z);
+							if (Local.x < light.Value.OGpos.x) {
+								light.Value.Light.transform.localPosition = new Vector3(light.Value.OGpos.x, Local.y, Local.z);
 							}
 						}
 						//Negative
-						else if (light.Value.MovementCap.x < 0){
-							if (Local.x < 0) {
+						else if (light.Value.MovementCap.x < light.Value.OGpos.x){
+							if (Local.x < light.Value.OGpos.x) {
 								light.Value.Light.transform.Translate(deltaT, 0, 0);
 							}
-							if (Local.x > 0) {
-								light.Value.Light.transform.localPosition = new Vector3(0, Local.y, Local.z);
+							if (Local.x > light.Value.OGpos.x) {
+								light.Value.Light.transform.localPosition = new Vector3(light.Value.OGpos.x, Local.y, Local.z);
 							}
 						}
 						///Z-axis
 						//Positive
-						if (light.Value.MovementCap.z > 0) { 
-							if (Local.z > 0) {
+						if (light.Value.MovementCap.z > light.Value.OGpos.z) { 
+							if (Local.z > light.Value.OGpos.z) {
 								light.Value.Light.transform.Translate(0, 0, -deltaT);
 							}
-							if (Local.z < 0) {
-								light.Value.Light.transform.localPosition = new Vector3(Local.x, Local.y, 0);
+							if (Local.z < light.Value.OGpos.z) {
+								light.Value.Light.transform.localPosition = new Vector3(Local.x, Local.y, light.Value.OGpos.z);
 							}
 						}
 						//Negative
-						else if (light.Value.MovementCap.z < 0){
-							if (Local.z < 0) {
+						else if (light.Value.MovementCap.z < light.Value.OGpos.z){
+							if (Local.z < light.Value.OGpos.z) {
 								light.Value.Light.transform.Translate(0, 0, deltaT);
 							}
-							if (Local.z > 0) {
-								light.Value.Light.transform.localPosition = new Vector3(Local.x, Local.y, 0);
+							if (Local.z > light.Value.OGpos.z) {
+								light.Value.Light.transform.localPosition = new Vector3(Local.x, Local.y, light.Value.OGpos.z);
 							}
 						}
 						break;
@@ -114,7 +121,7 @@ public class MovableLightingArray : SerializedMonoBehaviour {
 					case 3:
 						///X-axis
 						//Positive
-						if (light.Value.MovementCap.x > 0) { 
+						if (light.Value.MovementCap.x > light.Value.OGpos.x) { 
 							if (Local.x < light.Value.MovementCap.x) {
 								light.Value.Light.transform.Translate(deltaT, 0, 0);
 							}
@@ -123,7 +130,7 @@ public class MovableLightingArray : SerializedMonoBehaviour {
 							}
 						}
 						//Negative
-						else if (light.Value.MovementCap.x < 0){
+						else if (light.Value.MovementCap.x < light.Value.OGpos.x){
 							if (Local.x > light.Value.MovementCap.x) {
 								light.Value.Light.transform.Translate(-deltaT, 0, 0);
 							}
@@ -133,7 +140,7 @@ public class MovableLightingArray : SerializedMonoBehaviour {
 						}
 						///Z-axis
 						//Positive
-						if (light.Value.MovementCap.z > 0) { 
+						if (light.Value.MovementCap.z > light.Value.OGpos.z) { 
 							if (Local.z < light.Value.MovementCap.z) {
 								light.Value.Light.transform.Translate(0, 0, deltaT);
 							}
@@ -142,7 +149,7 @@ public class MovableLightingArray : SerializedMonoBehaviour {
 							}
 						}
 						//Negative
-						else if (light.Value.MovementCap.z < 0){
+						else if (light.Value.MovementCap.z < light.Value.OGpos.z){
 							if (Local.z > light.Value.MovementCap.z) {
 								light.Value.Light.transform.Translate(0, 0, -deltaT);
 							}
@@ -157,91 +164,93 @@ public class MovableLightingArray : SerializedMonoBehaviour {
 		}
 	}
 	[Button]
-	private void RotateDirection(bool _r) {
+	private void RotateDirection(int _r) {
 		float deltaT = (Time.deltaTime * RotSpeed);
 		//Go through all the lights to see which ones are selected.
 		foreach (var light in LightingTable) {
-			var Local = light.Value.Light.transform.localRotation.eulerAngles;
+			//Rotate the child object so that it wont interfere with the movement.
+			var Local = light.Value.Light.transform.GetChild(0).localRotation.eulerAngles;
 			//Which ever light is selected, compute the Inwards rotation on.
-			if (light.Value.Selected && _r) {
+			if (light.Value.Selected && _r == 1) {
 				///X axis
 				//Positive
 				if (light.Value.RotCap.x > 0) {
-					if (Local.x > 0 && Local.x <= (360 - light.Value.RotCap.x)) {
-						light.Value.Light.transform.Rotate(-deltaT, 0, 0);
+					if (Local.x > 0 && Local.x < (360 - light.Value.RotCap.x)) {
+						light.Value.Light.transform.GetChild(0).Rotate(-deltaT, 0, 0);
 					}
 					if (Local.x < 0 || Local.x > (360 - light.Value.RotCap.x)) {
-						light.Value.Light.transform.localRotation = Quaternion.Euler(0, 0, Local.z);
+						light.Value.Light.transform.GetChild(0).localRotation = Quaternion.Euler(0, 0, Local.z);
 					}
 				}
 				//Negative
 				else if (light.Value.RotCap.x < 0) {
 					if (Local.x < 0 || Local.x >= (360 + light.Value.RotCap.x)) {
-						light.Value.Light.transform.Rotate(deltaT, 0, 0);
+						light.Value.Light.transform.GetChild(0).Rotate(deltaT, 0, 0);
 					}
 					if (Local.x > 0 && Local.x < (360 + light.Value.RotCap.x)) {
-						light.Value.Light.transform.localRotation = Quaternion.Euler(0, 0, Local.z);
+						light.Value.Light.transform.GetChild(0).localRotation = Quaternion.Euler(0, 0, Local.z);
 					}
 				}
 				///Z axis
 				//Positive
 				if (light.Value.RotCap.y > 0) {
-					if (Local.z > 0 && Local.z <= (360 - light.Value.RotCap.y)) {
-						light.Value.Light.transform.Rotate(0, 0, -deltaT);
+					if (Local.z > 0 && Local.z < (360 - light.Value.RotCap.y)) {
+						light.Value.Light.transform.GetChild(0).Rotate(0, 0, -deltaT);
 					}
 					if (Local.z < 0 || Local.z > (360 - light.Value.RotCap.y)) {
-						light.Value.Light.transform.localRotation = Quaternion.Euler(Local.x, 0, 0);
+						light.Value.Light.transform.GetChild(0).localRotation = Quaternion.Euler(Local.x, 0, 0);
 					}
 				}
 				//Negative
 				else if (light.Value.RotCap.y < 0) {
-					if (Local.y < 0 || Local.z >= (360 + light.Value.RotCap.y)) {
-						light.Value.Light.transform.Rotate(0, 0, deltaT);
+					//Changed [Local.y < 0] to [(Local.z + deltaT) < 360 &&] to get the thing to not teloport.
+					if ((Local.z + deltaT) < 360 && Local.z >= (360 + light.Value.RotCap.y)) {
+						light.Value.Light.transform.GetChild(0).Rotate(0, 0, deltaT);
 					}
 					if (Local.y > 0 && Local.z < (360 + light.Value.RotCap.y)) {
-						light.Value.Light.transform.localRotation = Quaternion.Euler(Local.x, 0, 0);
+						light.Value.Light.transform.GetChild(0).localRotation = Quaternion.Euler((360 + light.Value.RotCap.y), 0, 0);
 					}
 				}
 			}
 			//Which ever light is selected, compute the Outwards rotation on.
-			else if (light.Value.Selected && !_r) {
+			else if (light.Value.Selected && _r == 2) {
 				///X axis
 				//Positive
 				if (light.Value.RotCap.x > 0) {
 					if (Local.x < light.Value.RotCap.x) {
-						light.Value.Light.transform.Rotate(deltaT, 0, 0);
+						light.Value.Light.transform.GetChild(0).Rotate(deltaT, 0, 0);
 					}
 					if (Local.x > light.Value.RotCap.x) { // && Local.x < (360-light.Value.RotCap.x)
-						light.Value.Light.transform.localRotation = Quaternion.Euler(light.Value.RotCap.x, 0, Local.z);
+						light.Value.Light.transform.GetChild(0).localRotation = Quaternion.Euler(light.Value.RotCap.x, 0, 0);
 					}
 				}
 				//Negative
 				//For some reason it switches it's Z axis sometimes but wont display that it did until it flips.
 				else if (light.Value.RotCap.x < 0) {
 					if (Local.x == 0 || Local.x > (360+light.Value.RotCap.x)) { // Local.x > light.Value.RotCap.x && 
-						light.Value.Light.transform.Rotate(-deltaT, 0, 0);
+						light.Value.Light.transform.GetChild(0).Rotate(-deltaT, 0, 0);
 					}
-					if (Local.x != 0 && Local.x < (360+light.Value.RotCap.x)) { //Local.x > light.Value.RotCap.x && 
-						light.Value.Light.transform.localRotation = Quaternion.Euler(light.Value.RotCap.x, 0, Local.z);
+					if (Local.x != 0 && Local.x <= (360+light.Value.RotCap.x)) { //Local.x > light.Value.RotCap.x && 
+						light.Value.Light.transform.GetChild(0).localRotation = Quaternion.Euler(light.Value.RotCap.x, 0, 0);
 					}
 				}
 				///Z axis
 				//Positive
 				if (light.Value.RotCap.y > 0) { 
 					if (Local.z < light.Value.RotCap.y) {
-						light.Value.Light.transform.Rotate(0, 0, deltaT);
+						light.Value.Light.transform.GetChild(0).Rotate(0, 0, deltaT);
 					}
 					if (Local.z > light.Value.RotCap.y) {
-						light.Value.Light.transform.localRotation = Quaternion.Euler(Local.x, 0, light.Value.RotCap.y);
+						light.Value.Light.transform.GetChild(0).localRotation = Quaternion.Euler(0, 0, light.Value.RotCap.y);
 					}
 				}
 				//Negative
 				else if (light.Value.RotCap.y < 0) {
 					if (Local.z == 0 || Local.z > (360+light.Value.RotCap.y)) {
-						light.Value.Light.transform.Rotate(0, 0, -deltaT);
+						light.Value.Light.transform.GetChild(0).Rotate(0, 0, -deltaT);
 					}
-					if (Local.z != 0 && Local.z < (360+light.Value.RotCap.y)) {
-						light.Value.Light.transform.localRotation = Quaternion.Euler(Local.x, 0, light.Value.RotCap.y);
+					if (Local.z != 0 && Local.z <= (360+light.Value.RotCap.y)) {
+						light.Value.Light.transform.GetChild(0).localRotation = Quaternion.Euler(0, 0, light.Value.RotCap.y);
 					}
 				}
 			}
@@ -253,6 +262,7 @@ public class MovableLightingArray : SerializedMonoBehaviour {
 		public GameObject Light;
 		public Vector2 RotCap;
 		public Vector3 MovementCap;
+		public Vector3 OGpos;
 	}
 	#endregion
 }
