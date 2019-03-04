dae611935414fa958598a90c5dc17d0217797a0f
jukejake <jacob.rosengren@uoit.net>
2019-03-03 21:34:21 -0500

    Added Lighting Array Mount model with 3 hinges.
    +Added Lighting Array Mount model.
    +Added Hinges connecting the 1x5 lights to the Mount.
    +Added a script MimicPosition for ease.

---
+++ Capstone weather visualization/Assets/MY/Scripts/MimicPosition.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class MimicPosition : SerializedMonoBehaviour {
+
+	#region Variables
+	public Transform ObjectToMimic;
+	[HorizontalGroup("X")]
+	public bool MimicX = false;
+	[HorizontalGroup("X")]
+	public float XOffset = 0.0f;
+	[HorizontalGroup("Y")]
+	public bool MimicY = false;
+	[HorizontalGroup("Y")]
+	public float YOffset = 0.0f;
+	[HorizontalGroup("Z")]
+	public bool MimicZ = false;
+	[HorizontalGroup("Z")]
+	public float ZOffset = 0.0f;
+	#endregion
+
+	#region Functions
+	void Start() {
+		
+	}
+	
+	void Update() {
+		Vector3 temp = this.transform.position;
+		if (MimicY) { temp.y = ObjectToMimic.position.y+YOffset; }
+		if (MimicX) { temp.x = ObjectToMimic.position.x+XOffset; }
+		if (MimicZ) { temp.z = ObjectToMimic.position.z+ZOffset; }
+		this.transform.position = temp;
+	}
+	#endregion
+}
